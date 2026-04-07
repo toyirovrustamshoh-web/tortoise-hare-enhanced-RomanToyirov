bc647508f636471a27c7b812458116c5dff53923
toyirovrustamshoh <toyirovrustamshoh@gmail.com>
2026-04-07 02:11:45 -0700

    chore: remove .history and .DS_Store from repo

--- .history/script_20260407011357.js
+++
@@ -1,6 +0,0 @@
-const track = document.getElementById("track");
-const message = document.getElementById("message");
-const startBtn = document.getElementById("startBtn");
-
-const tortoiseScoreEl = document.getElementById("tortoiseScore");
-const hareScoreEl = document.getElementById("hareScore");
